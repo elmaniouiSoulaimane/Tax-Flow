e00570e93c99b750763931c7450343513715f6bd
ELManioui Soulaimane <elmanioui.soulaimane@gmail.com>
2021-02-20 11:06:46 +0100

    added TaxeTNB & edited TerrainDao & TerrainService by Soulaiman

--- src/main/java/com/example/demo/dao/TerrainDao.java
+++ src/main/java/com/example/demo/dao/TerrainDao.java
@@ -1,7 +1,13 @@
 package com.example.demo.dao;
 
+import com.example.demo.bean.Terrain;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TerrainDao {
+public interface TerrainDao extends JpaRepository<Terrain, Long> {
+
+    Terrain findByReference(String reference);
+    Integer deleteByReference(String reference);
+    Integer updateByReference(String reference);
 }
