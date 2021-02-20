fd272a2f2348b86376a82f34ddc2011962463d7d
ELManioui Soulaimane <elmanioui.soulaimane@gmail.com>
2021-02-20 10:00:34 +0100

    init project by soulaimane bacha

---
+++ src/main/java/com/example/demo/bean/Terrain.java
@@ -0,0 +1,32 @@
+package com.example.demo.bean;
+
+
+public class Terrain {
+    private Integer id;
+    private String reference;
+    private String adresse;
+    private float surface;
+    private String categorie;
+
+
+    //GETTERS
+    public Integer getId() {
+        return id;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public float getSurface() {
+        return surface;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+}
